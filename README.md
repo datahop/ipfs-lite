f93c8c508aa4de8e8b9d093b8b72ea65b3847b99
asabya <asabya@gmail.com>
2021-04-07 16:56:22 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# IPFS-Lite
+
+IPFS-Lite is an embeddable, lightweight IPFS peer. This fork started from [ipfs-lite](https://github.com/hsanjuan/ipfs-lite).
+
+It offers all the features of main [ipfs-lite](https://github.com/hsanjuan/ipfs-lite). For certain requirements of datahop 
+it adds some more features of full ipfs, such as config, repo, leveldb etc.
+
+## Objectives
+
+* [ ] create cache repo as ipfs
+* [ ] have persistent config information (Id, keys, ports, bootstraps etc)
+* [ ] use leveldb as datastore to set up peer
+* [ ] generate gomobile binding from android
+
+## License
+
+Copyright 2021 Datahop
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ examples/litepeer/litepeer.go
@@ -0,0 +1,64 @@
+package main
+
+// This example launches an IPFS-Lite peer and fetches a hello-world
+// hash from the IPFS network.
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+
+	ipfslite "github.com/datahop/ipfs-lite"
+	"github.com/ipfs/go-cid"
+	crypto "github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/multiformats/go-multiaddr"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ds, err := ipfslite.BadgerDatastore("test")
+	if err != nil {
+		panic(err)
+	}
+	priv, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
+	if err != nil {
+		panic(err)
+	}
+
+	listen, _ := multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/4005")
+
+	h, dht, err := ipfslite.SetupLibp2p(
+		ctx,
+		priv,
+		nil,
+		[]multiaddr.Multiaddr{listen},
+		ds,
+		ipfslite.Libp2pOptionsExtra...,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	lite, err := ipfslite.New(ctx, ds, h, dht, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	lite.Bootstrap(ipfslite.DefaultBootstrapPeers())
+
+	c, _ := cid.Decode("QmWATWQ7fVPP2EFGu71UkfnqhYXDYH566qy47CnJDgvs8u")
+	rsc, err := lite.GetFile(ctx, c)
+	if err != nil {
+		panic(err)
+	}
+	defer rsc.Close()
+	content, err := ioutil.ReadAll(rsc)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(content))
+}
